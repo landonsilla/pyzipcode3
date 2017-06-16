84abc57ce04d17ad00b5030c11e5777ae117de92
Daniel van Flymen <vanflymen@gmail.com>
2017-06-16 14:50:53 -0400

    Update Setup

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-version = '1.2'
+version = '2.0'
 
 try:
     import sqlite3
@@ -11,9 +11,9 @@ else:
 
 setup(
     name='pyzipcode3',
-    version='1.2',
+    version=version,
     url='https://www.github.com/dvf/pyzipcode3',
-    description="Search by ZIP Code, City and Geo data",
+    description="Search by ZIP Code, Place and Geo data",
     keywords='zip code distance geo',
     author='Daniel van Flymen',
     author_email='vanflymen@gmail.com',
@@ -23,7 +23,7 @@ setup(
         'pyzipcode': ['zipcodes.db', ]
     },
     include_package_data=True,
-    download_url='https://www.github.com/dvf/pyzipcode3/archive/1.2.tar.gz',
+    download_url='https://www.github.com/dvf/pyzipcode3/archive/2.0.tar.gz',
     zip_safe=False,
     install_requires=requires,
     classifiers=[],
